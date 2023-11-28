41f0ffe3d1c41b5b9cc3936200b9168e48f27154
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-28 18:39:04 +0000

    task 0 correction

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-
 	new_node = malloc(sizeof(binary_tree_t));
 		if (!new_node)
 			return (NULL);
@@ -21,5 +20,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
         new_node->left = NULL;
 	new_node->right = NULL;
 
-        return (new_node);
+	return (new_node);
 }
