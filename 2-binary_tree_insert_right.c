852d4204ad8fce765a42d5692acba0308f17c694
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-28 19:23:20 +0000

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,30 +2,28 @@
 
 /**
  * binary_tree_insert_right - Inserts a node to the right of a binary tree
- * @parent: Parent of the node to be inserted
+ * @parent: Parent of the node to be  inserted
  * @value: Value of the new node
- * Return: Pointer to the new node, else NULL
+ * Return: Pointer to the new node, else NULL if failure
  */
-
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
 	if (!parent)
-	{			
+	{
 		return (NULL);
 	}
-        new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(parent, value);
 	if (!new_node)
 	{
 		return (NULL);
 	}
 	if (parent->right)
-        {
+	{
 		new_node->right = parent->right;
-		parent->right->parent = new_node
+		parent->right->parent = new_node;
 	}
-
 	parent->right = new_node;
 return (new_node);
 }
