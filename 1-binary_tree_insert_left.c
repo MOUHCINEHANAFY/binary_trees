229245611cf17ae295f13c6008f07eea897a46a5
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-28 19:01:20 +0000

    task 1 correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,32 +1,30 @@
 #include "binary_trees.h"
 
 /**
- * node_insert - Insert a new binary tree node
+ * node_insert - Creates a new binary tree node
  * @value: Value for n in the new node
  * Return: Pointer to the new node, or NULL on failure
  */
 
 binary_tree_t *node_insert(int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	return (new_node);
+return (new_node);
 }
 
 /**
  * binary_tree_insert_left - Inserts a node to the left of a binary tree
- * @parent: Inserted node parent
+ * @parent: Parent of the node to insert
  * @value: Value for n in the new node
  * Return: Pointer to the new node, or NULL on failure
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
@@ -41,5 +39,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = parent->left;
 	parent->left = new_node;
-	return (new_node);
+return (new_node);
 }
