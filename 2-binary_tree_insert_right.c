26c0b90537cea996b64806ffef80c55dd3e3eeee
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-28 19:17:53 +0000

    task 2 correction

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Inserts a node to the right of a binary tree
+ * binary_tree_insert_right - Inserts a node to the right of a binary tree
  * @parent: Parent of the node to be inserted
  * @value: Value of the new node
  * Return: Pointer to the new node, else NULL
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
