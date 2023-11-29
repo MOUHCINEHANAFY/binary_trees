a84a5d1501ca2c3090d14e6e5277c257b2773e29
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-29 20:16:33 +0000

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node->parent || !node)
+	if (!node || !node->parent)
 		return (NULL);
 	if (node->n < node->parent->n)
 		return (node->parent->right);
