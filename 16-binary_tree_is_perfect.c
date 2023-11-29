8959a67a77c3619b3432eb0a03f261f659fb57f0
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-11-29 20:05:35 +0000

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,59 +1,61 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - Check if a binary tree is perfect
+ * is_tree_perfect - Thus function counts the leaves in a binary tree
  * @tree: Binary tree
- * Return: 1 if it is perfect, 0 if not
+ * @depth: depth of left most side
+ * @level: depth of a chain
+ * Return: 1 if it is perfect 0 if not
+ */
+
+int is_tree_perfect(const binary_tree_t *tree, int depth, int level)
+{
+	if (!tree)
+		return (1);
+	if (!tree->right && !tree->left)
+	{
+		if (depth == (level + 1))
+			return (1);
+		return (0);
+	}
+	if (!tree->left || !tree->right)
+		return (0);
+	return (is_tree_perfect(tree->right, depth, level + 1) &&
+		is_tree_perfect(tree->left, depth, level + 1));
+}
+
+/**
+ * get_tree_depth - This function gets depth of left side
+ * @tree: binary tree
+ * Return: depth of tree
+ */
+
+int get_tree_depth(const binary_tree_t *tree)
+{
+	int tmp = 0;
+
+	while (tree)
+	{
+		tmp++;
+		tree = tree->left;
+	}
+	return (tmp);
+}
+
+
+/**
+ * binary_tree_is_perfect - This function check if binary tree is full
+ * @tree: binary tree
+ * Return: 1 if full 0 if not
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int depth = 0, level = 0;
+	int final_depth;
+
+	final_depth = get_tree_depth(tree);
 
 	if (!tree)
 		return (0);
-
-	while (tree)
-	{
-		depth++;
-		tree = tree->left;
-	}
-
-	tree = binary_tree_is_perfect_helper(tree, depth, &level);
-
-	return (tree != NULL);
-}
-
-/**
- * binary_tree_is_perfect_helper - Helper function for recursive check
- * @tree: Binary tree
- * @depth: Depth of the leftmost side
- * @level: Depth of a chain
- * Return: NULL if not perfect, otherwise, a pointer to the last checked node
- */
-const binary_tree_t *binary_tree_is_perfect_helper(
-	const binary_tree_t *tree, int depth, int *level)
-{
-	const binary_tree_t *left, *right;
-
-	if (!tree)
-		return (NULL);
-
-	if (!tree->left && !tree->right)
-	{
-		if (depth == (*level + 1))
-			return (tree);
-		return (NULL);
-	}
-
-	if (!tree->left || !tree->right)
-		return (NULL);
-
-	left = binary_tree_is_perfect_helper(tree->left, depth, level + 1);
-	right = binary_tree_is_perfect_helper(tree->right, depth, level + 1);
-
-	if (left && right)
-		return (right);
-
-	return (NULL);
+	return (is_tree_perfect(tree, final_depth, 0));
 }
